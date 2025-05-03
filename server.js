dd207d14fd8a3b57baaeb4167ae4bad17d97b14d
Tatakung <tasawa.012@gmail.com>
2025-05-03 12:02:58 +0700

    Fix: Update server.js for serverless deployment

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-app.use(cors());
-const app = express();
 const morgan = require("morgan");
 const { readdirSync } = require("fs");
+
+const app = express();
+
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json({ limit: "20mb" }));
 
-readdirSync("./routes").map((c) => app.use("/api/", require("./routes/" + c)));
+readdirSync("./routes").map((c) =>
+  app.use("/api/", require("./routes/" + c))
+);
 
-// app.listen(5000, () => {
-//   console.log("server runing");
-// });
 module.exports = app;
